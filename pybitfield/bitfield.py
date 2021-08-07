536cc347538d0a35a5a255a795f7d442c73125f8
cpyberry <cpyberry222@gmail.com>
2021-08-07 13:41:33 +0900

    feat: add Bitfield class constructor

--- pybitfield/bitfield.py
+++ pybitfield/bitfield.py
@@ -1,2 +1,9 @@
 class Bitfield:
-	pass
+	def __init__(self, number_of_element: int, bitfield=0):
+		"""
+		Args:
+			number_of_element (int): fixed length bitfield size.
+			bitfield (int, optional): initial value of bitfield. Defaults to 0.
+		"""
+		self.number_of_element = number_of_element
+		self.bitfield = bitfield
